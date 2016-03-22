8f061681bd2d045dada170703ed3ea921035f14b
Jack Force <jack1243star@gmail.com>
2016-03-23 06:51:23 +0800

    Working implementation, fix #3

--- source_coding/test_arith.c
+++ source_coding/test_arith.c
@@ -26,7 +26,6 @@ thought Alice `without pictures or conversation ? '";
 
 TEST arith_encode_decode_sum()
 {
-	size_t i;
 	unsigned char input[38420];
 	size_t encoded_bits;
 	unsigned char * encoded;
@@ -38,11 +37,28 @@ TEST arith_encode_decode_sum()
 	encoded = arith_encode(input, &encoded_bits, 38420);
 	arith_decode(encoded, decoded, 38420);
 
-	for (i = 0; i < 38420; i++)
-	{
-		printf("Asserting input[%u] = 0x%x\n", i, input[i]);
-		ASSERT_EQ(input[i], decoded[i]);
-	}
+	ASSERT_EQ(0, memcmp(input, decoded, 38420));
+	PASS();
+}
+
+TEST arith_encode_decode_kennedy()
+{
+	unsigned char * input;
+	size_t encoded_bits;
+	unsigned char * encoded;
+	unsigned char * decoded;
+
+	input = (unsigned char *)malloc(1029744);
+	decoded = (unsigned char *)malloc(1029744);
+
+	FILE* fp = fopen("cantrbry/kennedy.xls", "rb");
+	fread(input, 1, 1029744, fp);
+	fclose(fp);
+
+	encoded = arith_encode(input, &encoded_bits, 1029744);
+	arith_decode(encoded, decoded, 1029744);
+
+	ASSERT_EQ(0, memcmp(input, decoded, 1029744));
 	PASS();
 }
 
@@ -51,4 +67,5 @@ SUITE(arith_suite)
 	/* Compress and decompress */
 	RUN_TEST(arith_encode_decode_alice1);
 	RUN_TEST(arith_encode_decode_sum);
+	RUN_TEST(arith_encode_decode_kennedy);
 }
